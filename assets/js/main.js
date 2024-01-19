997db79127d7323d564690981ff2e1c9efa9666a
LinaRedfox <vdovichenko.lv@mail.ru>
2024-01-19 23:16:07 +0300

    main changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,18 +31,164 @@ const scrollHeader = () => {
         : header.classList.remove('bg-header')
 }
 
-window.addEventListener('scroll',scrollHeader)
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+    calculateCm = document.getElementById('calculate-cm'),
+    calculateKg = document.getElementById('calculate-kg'),
+    calculateMessage = document.getElementById('calculate-message')
 
+const calculateBmi = (e) => {
+    e.preventDefault()
+
+    // Check if the fields have a value
+    if (calculateCm.value === '' || calculateKg === '') {
+        // Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        // Show message
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+
+        // Remove message three seconds
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else {
+        // BMI Formula
+        const cm = calculateCm.value / 100,
+            kg = calculateKg.value,
+            bmi = Math.round(kg / (cm * cm))
+
+        //Show health status
+        if (bmi < 18.5) {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are skinny 😔'
+        }
+
+        else if (bmi < 25) {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are healthy 😄'
+        }
+
+        else {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are overweight 😑'
+        }
+
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+}
+
+calculateForm.addEventListener('submit', calculateBmi)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // Check if the field has a value
+    if (contactUser.value === '') {
+        // Add and remove color
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        // Show message
+        contactMessage.textContent = 'You must enter your email ❌'
+
+        // Remove message three seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 3000)
+    }
+    else {
+        // serviceID - templateID - #form - publicKey
+        emailjs.send('service_jrwsjls', template_6n0eygr, '#contact-form', vFCarzgKK2e - rhvb6)
+            .then(() => {
+                // Show message and add color
+                contactMessag.classList.add('color-green')
+                contactMessage.textContent = 'You registered successfully ✅'
+
+                // Remove message three seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 3000)
+            }, (error) => {
+                // Mail sending error
+               alert('OOPS! SOMETHING HAS FAILED...', error)
+            })
+
+            // To clear the input field
+            contactUser.value = ''
+    }
+
+
+}
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance:'60px',
+    duration: 2500,
+    delay: 400,
+    reset: true,   //Animations repeat 
+})
+
+sr.reveal('home_data, .footer_container, .footer_group')
+sr.reveal('home_img',{delay: 700, origin: 'bottom'})
+sr.reveal('.choose_img, .calculate_content', {origin: 'left'})
+sr.reveal('.choose_content,.calculate_img', {origin:'right'})
+sr.reveal('.logos_img,.program_card,.pricing_card',{interval: 150})
+
+
+
+
